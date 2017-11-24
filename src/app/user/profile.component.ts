b7de726dfdbb4bd5b832e9ade08a3c785234ee03
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2017-11-24 20:14:17 +0200

    chore .....

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: './profile.component.html',
+  styles: [`
+    em {float: right; color: #e05c65; padding-left: 10px;}
+    .error input {background-color: #e3c3c5;}
+    .error ::-webkit-input-placeholder {color: #999;}
+  `]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit() {
+    let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required);
+    let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required);
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
 
+  saveProfile(formValues) {
+    if(this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['events']);
+    }
+
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
+  }
 }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { ProfileComponent } from "./profile.component";
 import { userRoutes } from "./user.routes";
@@ -9,7 +9,7 @@ import { LoginComponent } from "./login.component";
 
 @NgModule({
   imports: [
-    CommonModule, FormsModule, RouterModule.forChild(userRoutes)
+    CommonModule, FormsModule, ReactiveFormsModule, RouterModule.forChild(userRoutes)
   ],
   declarations: [
     ProfileComponent, LoginComponent
